2cdf1fdbafe880f1d880a41a1f447de76199ae31
Abheeshskurup <abheeshskurup@gmail.com>
2025-01-31 08:30:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMembers() {
+    return [
+        { name: 'Suzanne', skills: ['JavaScript', 'React', 'Node'] },
+        { name: 'John', skills: ['JavaScript', 'PHP', 'Docker'] },
+        { name: 'Joseph', skills: ['HTML', 'CSS', 'JavaScript'] },
+        { name: 'Alex', skills: ['React', 'Redux', 'Node'] },
+        { name: 'Tom', skills: ['React', 'Redux', 'Node'] },
+    ];
+}
