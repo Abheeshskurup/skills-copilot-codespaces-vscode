406f134b8ddccc8033bdeb39db5dcb13476f455c
Abheeshskurup <abheeshskurup@gmail.com>
2025-01-31 08:38:27 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,61 @@
-//create web server
+//Create web server
 const express = require('express');
 const app = express();
-app.use(express.json());
-const port = 3000;
-const commentData = [
-    {
-        id: 1,
-        comment: "This is the first comment"
-    },
-    {
-        id: 2,
-        comment: "This is the second comment"
-    },
-    {
-        id: 3,
-        comment: "This is the third comment"
-    }
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+// Create a list of comments
+let comments = [
+  { 
+    id: 1,
+    username: 'Alice',
+    comment: 'I love this website!'
+  },
+  { 
+    id: 2,
+    username: 'Bob',
+    comment: 'This website sucks!'
+  }
 ];
-app.get('/comments', (req, res) => {
-    res.send(commentData);
+
+// Get all comments
+app.get('/api/comments', (req, res) => {
+  res.send(comments);
 });
-app.post('/comments', (req, res) => {
-    const newComment = req.body;
-    commentData.push(newComment);
-    res.send(commentData);
+
+// Add a new comment
+app.post('/api/comments', (req, res) => {
+  const comment = {
+    id: comments.length + 1,
+    username: req.body.username,
+    comment: req.body.comment
+  };
+  comments.push(comment);
+  res.send(comment);
 });
+
+// Delete a comment
+app.delete('/api/comments/:id', (req, res) => {
+  const index = comments.findIndex(comment => comment.id === parseInt(req.params.id));
+  if (index !== -1) {
+    comments.splice(index, 1);
+  }
+  res.send();
+});
+
+// Update a comment
+app.put('/api/comments/:id', (req, res) => {
+  const index = comments.findIndex(comment => comment.id === parseInt(req.params.id));
+  if (index !== -1) {
+    comments[index].username = req.body.username;
+    comments[index].comment = req.body.comment;
+  }
+  res.send(comments[index]);
+});
+
+const port = 3000;
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is listening on http://localhost:${port}`);
 });
